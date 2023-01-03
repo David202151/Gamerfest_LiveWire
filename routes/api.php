a5a407f397716d16ddfa8bf7c8dd66c8b8524d2e
DaliPazmino <dalilapazmino@gmail.com>
2023-01-02 20:20:12 -0500

    api videojuego

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\JugadoresController as JugadoresV1;
+use App\Http\Controllers\Api\V1\VideojuegoController as VideojuegoV1;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -23,5 +24,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::apiResource('v1/jugadores', JugadoresV1::class)
       ->only(['index','show', 'destroy'])
       ->middleware('auth:sanctum');
+
+Route::apiResource('v1/videojuegos', VideojuegoV1::class)
+      ->only(['index','show', 'destroy'])
+      ->middleware('auth:sanctum');
+
 
 Route::post('login', [App\Http\Controllers\Api\LoginController::class, 'login']);
