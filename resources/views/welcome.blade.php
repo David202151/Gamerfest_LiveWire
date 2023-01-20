316abbbe8144d4347fb36f6ce95743e13a6a2381
Kenneth0101 <katoapanta4@espe.edu.ec>
2023-01-19 22:42:02 -0500

    Vercion final

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -42,13 +42,8 @@
                 </li>
             </ul>
             <ul>
-<<<<<<< HEAD
-                <li class="nav-link">
-                <a href="#"><h5 class="text-white">Productos</h5></a>
-=======
                 <li class="nav-item">
                 <a href="{{ url('views/productos.blade.php') }}"><h5 class="text-white">Productos</h5></a>
->>>>>>> main
                 </li>
             </ul>
             <ul>
@@ -57,13 +52,9 @@
                 </li>
             </ul>
             <ul>
-<<<<<<< HEAD
-                <li class="nav-link">
-                <a href="#"><h5 class="text-white">Normativa del evento</h5></a>
-=======
+
                 <li class="nav-item">
                 <a href="{{ url('views/normativa.blade.php') }}"><h5 class="text-white">Normativa del evento</h5></a>
->>>>>>> main
                 </li>
             </ul>
             </div>
